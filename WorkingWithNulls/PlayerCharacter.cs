788849bd4cd93d4dad5a637fb03a69bbaa74f995
Mike V <dev.mikev@gmail.com>
2018-11-07 14:09:09 -0500

    Implement Null Object Pattern with Interface

---
+++ WorkingWithNulls/PlayerCharacter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithNulls
+{
+    class PlayerCharacter
+    {
+        private readonly ISpecialDefense _specialDefense;
+
+        public PlayerCharacter(ISpecialDefense specialDefense)
+        {
+            _specialDefense = specialDefense;
+        }
+
+        public string Name { get; set; }
+        public int Health { get; set; } = 100;
+
+        public void Hit(int damage)
+        {
+        //Part 1
+            //int damageReduction = 0;
+
+            //if (_specialDefense != null)
+            //{
+            //damageReduction = _specialDefense.CalculateDamageReduction(damage);
+            //}
+
+            //int totalDamageTaken = damage - damageReduction;
+        //End part 1
+
+        //Part 2
+            int totalDamageTaken = damage - _specialDefense.CalculateDamageReduction(damage);
+        //End part 2
+
+            Health -= totalDamageTaken;
+
+            Console.WriteLine($"{Name}'s health has been reduced by {totalDamageTaken} to {Health}.");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Part 1
+        //public string Name { get; set; }
+        //public int? DaysSinceLastLogin { get; set; }
+        //public DateTime? DateOfBirth { get; set; }
+        //public bool? IsNoob { get; set; }
+
+        //public PlayerCharacter()
+        //{
+        //    DateOfBirth = null;
+        //    DaysSinceLastLogin = null;
+        //}
+    }
+}
---
+++ WorkingWithNulls/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithNulls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 4
+
+            PlayerCharacter sarah = new PlayerCharacter(new DiamondSkinDefense())
+            {
+                Name = "Sarah"
+            };
+
+            PlayerCharacter amrit = new PlayerCharacter(new IIronBonesDefense())
+            {
+                Name = "Amrit"
+            };
+
+            PlayerCharacter gentry = new PlayerCharacter(new NullDefense())
+            {
+                Name = "Gentry"
+            };
+
+
+            sarah.Hit(10);
+            amrit.Hit(10);
+            gentry.Hit(10);
+
+            Console.ReadLine();
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Part 3
+            //PlayerCharacter[] players = null;
+
+            //string p1 = players?[0]?.Name;
+            //string p2 = players?[1]?.Name;
+            //string p3 = players?[2]?.Name;
+
+            //Console.ReadLine();
+
+         // Part 2
+            //PlayerCharacter[] players = new PlayerCharacter[3]
+            //{
+            //    new PlayerCharacter {Name = "Sarah"},
+            //    new PlayerCharacter(), // Name = null
+            //    null // PlayerCharacter = null
+            //};
+
+
+         //Part 1
+            // *******************************************************
+            //PlayerCharacter player = new PlayerCharacter();
+            ////player.DaysSinceLastLogin = 42;
+
+            ////int days = player.DaysSinceLastLogin.Value;
+            //int days = player?.DaysSinceLastLogin ?? -1;
+
+            //Console.WriteLine(days);
+
+            //Console.ReadLine();
+        }
+    }
+}
